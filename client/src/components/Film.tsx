183734d606da1cdcba1f7f27e37d53cf602ed8c7
Titouan Escorneboueu <titouanescorneboueu@gmail.com>
2022-05-10 11:56:53 +0200

    Call to api for recover all movies

--- client/src/components/Film.tsx
+++ client/src/components/Film.tsx
@@ -9,10 +9,13 @@ const Film = ({ film }: FilmProps) => {
   return (
     <>
       {film?.map((value: IFilms, index: number, array: IOneFilm) => {
+        console.log(value);
+        console.log(index);
+        console.log(array);
         return (
-          <main key={value.Id}>
+          <main key={index}>
             <h2>{value.Title}</h2>
-            <img src={value.Image} alt="Image du film" />
+            <img src={value.Image} alt="Affiche du film" />
             <p>{value.Description}</p>
           </main>
         );
--- client/src/pages/Films.tsx
+++ client/src/pages/Films.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Film from "../components/Film";
 import NavBar from "../components/NavBar";
 import { IOneFilm } from "../interfaces/App_interface";
+import axios from "axios";
 
 interface FilmsProps {
   film: IOneFilm | any;
@@ -10,6 +11,26 @@ interface FilmsProps {
 const Films = () => {
   const [appFilm, setAppFilm] = useState<FilmsProps>();
 
+  const getMovies = async (): Promise<IOneFilm> =>
+    new Promise((resolve, reject) => {
+      try {
+        axios
+          .post(
+            `${process.env.REACT_APP_API_URL}/movies`,
+            JSON.stringify({ page: 1 })
+          )
+          .then((res) => {
+            resolve(res.data);
+            setAppFilm(res.data);
+          });
+      } catch (error) {
+        reject(error);
+      }
+    });
+
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <main className="films">
       <h1>Tout les films</h1>
